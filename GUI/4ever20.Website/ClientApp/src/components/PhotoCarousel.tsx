14d04cce1b7247eff146d10a8e54799df49225d3
Daniil Bladyko <daniil.bladyko@gmail.com>
2020-06-14 11:57:57 +0200

    Add photo carousel

---
+++ GUI/4ever20.Website/ClientApp/src/components/PhotoCarousel.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from 'react';
+import { connect } from 'react-redux';
+import { UncontrolledCarousel } from 'reactstrap';
+import photo1 from '../photos/Forever_twenty_Moment.jpg';
+import photo2 from '../photos/Still_Models_1.jpg';
+import photo3 from '../photos/Still_Models_2.jpg';
+
+const items = [
+    { src: photo1, altText: 'Photo 1', key: '1' },
+    { src: photo2, altText: 'Photo 2', key: '2' },
+    { src: photo3, altText: 'Photo 3', key: '3' }
+];
+
+const PhotoCarousel = () => <UncontrolledCarousel items={items} />;
+
+export default connect()(PhotoCarousel);
